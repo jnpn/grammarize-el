b07d2175e76ecc46a649bf241d40c73eb4bf0694
Johan PONIN <johan.ponin.pro@gmail.com>
2014-12-05 21:29:20 +0100

    added a -treecount function

--- grammarize.el
+++ grammarize.el
@@ -56,7 +56,12 @@ TREE: input tree"
 (-map #'xml-descendancy (-walk #'xml-nodefn #'xml-childrenfn *xml*))
 
 (-walk #'xml-nodefn #'xml-childrenfn *xml*)
+(defun -treecount (tree)
+  "Count the element of TREE."
+  (apply #'+ (-walk (lambda (t) 1) #'xml-childrenfn tree)))
 
+(-treecount *xml*)
+;;; -> 29
 
 ;;; Various Tests:
 
